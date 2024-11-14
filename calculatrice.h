31da9418fb9dd639aa0be0235f43db997c0ad3fe
GBENCHEIKH <ghita.bencheikh@gmail.com>
2024-11-14 15:31:40 +0100

    Création de la class calculatrice avec des tests unitaires

---
+++ calculatrice.h
@@ -0,0 +1,12 @@
+#ifndef CALCULATRICE_H
+#define CALCULATRICE_H
+
+class Calculatrice{
+    public : 
+    static double addition(double x, double y);
+    static double soustraction(double x, double y);
+    static double multiplication(double x, double y);
+    static double division(double x, double y);
+};
+
+#endif
---
+++ main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include "calculatrice.h"
+#include <gtest/gtest.h>
+
+using namespace std; 
+
+// Test de la méthode addition
+TEST(main, Addition) {
+    EXPECT_DOUBLE_EQ(Calculatrice::addition(0.5, 0.1), 0.6);
+    EXPECT_DOUBLE_EQ(Calculatrice::addition(-1.0, 1.0), 0.0);
+    EXPECT_DOUBLE_EQ(Calculatrice::addition(2.0, 2.0), 4.0);
+}
+
+// Test de la méthode soustraction
+TEST(main, Soustraction) {
+    EXPECT_DOUBLE_EQ(Calculatrice::soustraction(0.5, 0.1), 0.4);
+    EXPECT_DOUBLE_EQ(Calculatrice::soustraction(-1.0, 1.0), -2.0);
+    EXPECT_DOUBLE_EQ(Calculatrice::soustraction(2.0, 2.0), 0.0);
+}
+
+// Test de la méthode multiplication
+TEST(CalculatriceTest, Multiplication) {
+    EXPECT_DOUBLE_EQ(Calculatrice::multiplication(0.5, 0.1), 0.05);
+    EXPECT_DOUBLE_EQ(Calculatrice::multiplication(-1.0, 1.0), -1.0);
+    EXPECT_DOUBLE_EQ(Calculatrice::multiplication(2.0, 2.0), 4.0);
+}
+
+// Test de la méthode division
+TEST(CalculatriceTest, Division) {
+    EXPECT_DOUBLE_EQ(Calculatrice::division(0.5, 0.1), 5.0);
+    EXPECT_DOUBLE_EQ(Calculatrice::division(-1.0, 1.0), -1.0);
+    EXPECT_DOUBLE_EQ(Calculatrice::division(2.0, 2.0), 1.0);
+
+    // Test de la division par zéro
+    EXPECT_THROW(Calculatrice::division(1.0, 0.0), std::invalid_argument);
+}
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
